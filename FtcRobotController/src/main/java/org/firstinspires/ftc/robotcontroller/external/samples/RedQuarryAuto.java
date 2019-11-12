faf6af77a93582f73f4d1a1ff1205c29072aa629
Natick5436 <47368008+Natick5436@users.noreply.github.com>
2019-11-12 17:29:22 -0500

    Add file

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RedQuarryAuto.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RedQuarryAuto.java
@@ -1,7 +1,8 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
+
+//This is a test class for GitHub use only, it is a placeholder for the actual Autonomous Code.
 public class RedQuarryAuto {
-    private double now;
-    private double you;
+
 
 }
