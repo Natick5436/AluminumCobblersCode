8ece30c0f1fe388320ceacb5951d3f9b57b978a0
Natick5436 <47368008+Natick5436@users.noreply.github.com>
2019-11-12 17:17:14 -0500

    Add existing file

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RedQuarryAuto.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+public class RedQuarryAuto {
+    private double now;
+    private double you;
+
+}
--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/TestClass.java
+++
@@ -1,6 +0,0 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
-
-public class TestClass {
-    private double go;
-
-}
