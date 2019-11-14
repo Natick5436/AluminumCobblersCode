a3b8dca27ef80b59857d754f17097e6ebe34c9d4
Natick5436 <47368008+Natick5436@users.noreply.github.com>
2019-11-14 14:24:27 -0500

    changes

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RedQuarryAuto.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/RedQuarryAuto.java
@@ -3,5 +3,6 @@ package org.firstinspires.ftc.robotcontroller.external.samples;
 
 //This is a test class for GitHub use only, it is a placeholder for the actual Autonomous Code. 5436 has successfully created a repository.
 public class RedQuarryAuto {
+    private double aiden;
 
 }
